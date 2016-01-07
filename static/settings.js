50d99bcc8d7d3fe01efc370443ea0f02110884c5
Sally <sallybao29@gmail.com>
2016-01-06 22:43:02 -0500

    downloaded FlatUI

--- static/settings.js
+++ static/settings.js
@@ -1,23 +1,23 @@
 console.log("These are the settings");
-
-var settings = function settings(){
-		var button = document.getElementById("settings");
-		button.addEventListener("click", function(e){
-				console.log(this);
-				this.children[1].classList.toggle("show");
-		});
+/*
+	var settings = function settings(){
+	var button = document.getElementById("settings");
+	button.addEventListener("click", function(e){
+	console.log(this);
+	this.children[1].classList.toggle("show");
+	});
 		
-		window.addEventListener("click", function(e){
-				if (!e.target.matches('.dropbtn')){
-						var options = document.getElementsByClassName("dropdown-content");
-						for (var i = 0; i < options.length; i++){
-								if (options[i].classList.contains('show'))
-										options[i].classList.remove('show');
-						}
-				}
-		});
-};
+	window.addEventListener("click", function(e){
+	if (!e.target.matches('.dropbtn')){
+	var options = document.getElementsByClassName("dropdown-content");
+	for (var i = 0; i < options.length; i++){
+	if (options[i].classList.contains('show'))
+	options[i].classList.remove('show');
+	}
+	}
+	});
+	};
+
+*/
 
 
-
-settings();
