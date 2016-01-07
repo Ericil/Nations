50eb544235165e60b476836e140b83bb6891f165
Sally <sallybao29@gmail.com>
2016-01-06 19:35:52 -0500

    settings

--- static/settings.js
+++ static/settings.js
@@ -1,15 +1,23 @@
 console.log("These are the settings");
 
-
-
-
-var logout = function logout(){
-    $.get("/logout", function(data){
-	console.log("Successfully logged out");
-    });
+var settings = function settings(){
+		var button = document.getElementById("settings");
+		button.addEventListener("click", function(e){
+				console.log(this);
+				this.children[1].classList.toggle("show");
+		});
+		
+		window.addEventListener("click", function(e){
+				if (!e.target.matches('.dropbtn')){
+						var options = document.getElementsByClassName("dropdown-content");
+						for (var i = 0; i < options.length; i++){
+								if (options[i].classList.contains('show'))
+										options[i].classList.remove('show');
+						}
+				}
+		});
 };
 
-var accountSettings = function accountSettings(){
-    
 
-};
+
+settings();
