607499633729b8e7b12119efc8579ce659500baf
Sally Bao <sallybao29@gmail.com>
2016-01-07 20:34:20 -0500

    making sidebar

--- static/settings.js
+++ static/settings.js
@@ -1,23 +1,16 @@
 console.log("These are the settings");
-/*
-	var settings = function settings(){
-	var button = document.getElementById("settings");
-	button.addEventListener("click", function(e){
-	console.log(this);
-	this.children[1].classList.toggle("show");
-	});
-		
-	window.addEventListener("click", function(e){
-	if (!e.target.matches('.dropbtn')){
-	var options = document.getElementsByClassName("dropdown-content");
-	for (var i = 0; i < options.length; i++){
-	if (options[i].classList.contains('show'))
-	options[i].classList.remove('show');
-	}
-	}
-	});
-	};
 
-*/
+var makeOptions = function makeOptions(){
+    var button = document.createElement("button");
+    button.type = "button";
+    button.className = "btn btn-primary btn-circle btn-lg";
+    var icon = document.createElement("span");
+    icon.className = "glyphicon glyphicon-list";
+
+    button.appendChild(icon);
+    document.getElementById("sideoptions").appendChild(button);
+
+};
 
 
+makeOptions();
