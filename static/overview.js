7a629a3ab65feba4289a567f5a4c6fce3932549d
Sally Bao <sallybao29@gmail.com>
2016-01-08 19:53:37 -0500

    working on overview

---
+++ static/overview.js
@@ -0,0 +1,9 @@
+console.log("Here is the overview");
+
+var makeOverview = function makeOverview(){
+    var form = document.createElement("form");
+    form.id = "overview";
+    form.classList.add("popup");
+
+};
+
--- static/settings.js
+++ static/settings.js
@@ -1,32 +1,23 @@
 console.log("These are the settings");
 
 var makeOptions = function makeOptions(){
-    var button = document.createElement("button");
-    button.type = "button";
-    button.className = "btn btn-primary btn-circle";
-    var icon = document.createElement("span");
-    icon.className = "glyphicon glyphicon-list";
+    $("#options").mouseover(function(){
+	$(".btn-group-vertical button").each(function(i){
+	    console.log(this);
+	    if (i > 0){
+		$(this).fadeIn();
+	    }
+	});
+    });
 
-    button.appendChild(icon);
-    document.getElementById("sideoptions").appendChild(button);
-
+    $("#sideoptions").mouseleave(function(){
+	$(".btn-group-vertical button").each(function(i){
+	    console.log(this);
+	    if (i > 0){
+		$(this).fadeOut();
+	    }
+	});
+    });
 };
 
-
-$("#options").mouseover(function(){
-		$(".btn-group-vertical button").each(function(i){
-				console.log(this);
-				if (i > 0){
-						$(this).fadeIn();
-				}
-		});
-});
-
-$("#sideoptions").mouseleave(function(){
-		$(".btn-group-vertical button").each(function(i){
-				console.log(this);
-				if (i > 0){
-						$(this).fadeOut();
-				}
-		});
-});
+makeOptions();
