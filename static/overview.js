dd3e5293ab952673ca86c80cc27d9c33f890731c
Sally <sallybao29@gmail.com>
2016-01-08 23:30:27 -0500

    worked on overview

--- static/overview.js
+++ static/overview.js
@@ -1,9 +1,6 @@
 console.log("Here is the overview");
 
-var makeOverview = function makeOverview(){
-    var form = document.createElement("form");
-    form.id = "overview";
-    form.classList.add("popup");
-
+var overview = function makeOverview(){
+   
 };
 
--- static/settings.js
+++ static/settings.js
@@ -1,23 +1,58 @@
 console.log("These are the settings");
 
-var makeOptions = function makeOptions(){
+/*Adds mouseover events to buttons
+	Mouse over and out on options button to view options
+	Mouse over options to view descriptions
+*/
+var hoverOptions = function hoverOptions(){
     $("#options").mouseover(function(){
-	$(".btn-group-vertical button").each(function(i){
-	    console.log(this);
-	    if (i > 0){
-		$(this).fadeIn();
-	    }
-	});
-    });
+				$(".btn-group-vertical button").each(function(i){
+						if (i > 0)
+								$(this).fadeIn();
+				});
+		});
 
-    $("#sideoptions").mouseleave(function(){
-	$(".btn-group-vertical button").each(function(i){
-	    console.log(this);
-	    if (i > 0){
-		$(this).fadeOut();
-	    }
-	});
-    });
+		$("#sideoptions").mouseleave(function(){
+				$(".btn-group-vertical button").each(function(i){
+						if (i > 0)
+								$(this).fadeOut();	
+				});
+		});
+		
+		$('[data-toggle="tooltip"]').tooltip();
 };
 
-makeOptions();
+
+/*Adds click events to buttons
+	Run a different function depending on the button
+*/
+var clickOptions = function clickOptions(){
+		var options = document.getElementById("sideoptions");
+		options.addEventListener("click", function(e){
+				switch(e.target.id) {
+				case "overview":
+						showOverview();
+						break;
+				case "message":
+						message();
+						break;
+				case "buildinfo":
+						showBuildInfo();
+						break;
+				case "build":
+						build();
+				}
+		});
+};
+
+
+/*Brings up the city overview screen
+ */
+var showOverview = function showOverview(){
+		var form = document.getElementById("overview-popup");
+		form.style.display = "block";
+};
+
+
+hoverOptions();
+clickOptions();
