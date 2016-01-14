03fe81c5a3b7c98ef685a73c823c1a4175e7e726
Sally <sallybao29@gmail.com>
2016-01-13 23:58:02 -0500

    worked on chat

--- static/settings.js
+++ static/settings.js
@@ -22,62 +22,68 @@ var hoverOptions = function hoverOptions(){
     $('[data-toggle="tooltip"]').tooltip();
 };
 
+var addText = function addText(e){
+		if (e.keyCode == 13){
+				e.preventDefault();
+				var input = document.getElementById("chat-input");
+				if (input.value && input.value.trim()){	
+						var body = document.getElementById("chat-body");
+						body.innerHTML += input.value.trim() + "<br/>";
+						input.value = "";
+						body.scrollTop = body.scrollHeight - body.clientHeight;
+				}
+		}
+
+};
+
 /*Adds closing capability
-  Adds text input to chat body
+	Adds text input to chat body
 */
 var chatSetup = function chatSetup(){
-    $("#chat-close").click(function(e){
-	$("#chatbox").css("display", "none");
-    });
+		$("#chat-close").click(function(e){
+				$("#chatbox").css("display", "none");
+		});
     
-    var text = document.getElementById("chat-input");
-    text.addEventListener("keydown", function(e){
-	if (e.keyCode == 13){
-	    e.preventDefault();
-	    if (this.value && this.value.trim()){	
-		var body = document.getElementById("chat-body");
-		body.innerHTML += this.value.trim()  + '<br>';
-		this.value = "";
-		body.scrollTop = body.scrollHeight - body.clientHeight;
-	    }
-	}
-    })
+		var text = document.getElementById("chat-input");
+		text.addEventListener("keydown", function(e){
+				addText(e);
+		})
 };
 
 /*Adds click events to buttons
-  Run a different function depending on the button
+	Run a different function depending on the button
 */
 var clickOptions = function clickOptions(){
-    var options = document.getElementById("sideoptions");
-    options.addEventListener("click", function(e){
-	console.log(e.target.id);
-	switch(e.target.id) {
-	case "overview":
-	    showOverview();
-	    break;
-	case "message":
-	    message();
-	    break;
-	case "buildinfo":
-	    showBuildInfo();
-	    break;
-	case "build":
-	    build();
-	}
-    });
+		var options = document.getElementById("sideoptions");
+		options.addEventListener("click", function(e){
+				console.log(e.target.id);
+				switch(e.target.id) {
+				case "overview":
+						showOverview();
+						break;
+				case "message":
+						message();
+						break;
+				case "buildinfo":
+						showBuildInfo();
+						break;
+				case "build":
+						build();
+				}
+		});
 };
 
 
 /*Brings up the city overview screen
  */
 var showOverview = function showOverview(){
-    $('#overview-form').modal();
+		$('#overview-form').modal();
 };
 
 
 var message = function message(){
-    var chatbox = document.getElementById("chatbox");
-    chatbox.style.display = "block";
+		var chatbox = document.getElementById("chatbox");
+		chatbox.style.display = "block";
 };
 
 
