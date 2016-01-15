56123441f7005431d456f1cf9b255a58b132ecc7
imgntn159 <imgtn159@gmail.com>
2016-01-15 16:59:22 +0000

    cleanup

--- dev-test/game.js
+++ dev-test/game.js
@@ -2,7 +2,8 @@ Crafty.init(750, 350, document.getElementById('game'));
 //use a div with id="game", first two numbers are pixel dimensions
 Crafty.sprite(128, "sprite.png", {
   grass: [0,0,1,1],
-  stone: [1,0,1,1]
+  stone: [1,0,1,1],
+  build: [2,0,1,1]
 });
 
 
@@ -27,14 +28,14 @@ for(var i = 5; i >= 0; i--) {
 }
 
 function generate(thingy){
-	var tile = Crafty.e("2D, DOM, stone, Mouse")
+	var tile = Crafty.e("2D, DOM, build, Mouse")
 	.attr('z', (thingy.xCord+1 * thingy.yCord+1))
 	.areaMap([64,0],[128,32],[128,96],[64,128],[0,96],[0,32])
 	.bind("MouseOver", function(){
-		this.sprite(1,1,1,1);//select sprite
+		this.sprite(2,1,1,1);//select sprite
 		bugtest(this);
 	}).bind("MouseOut", function(){
-		this.sprite(1,0,1,1);//regular sprite
+		this.sprite(2,0,1,1);//regular sprite
 		document.getElementById("potato").innerHTML = "shhhh"
 	}).bind("Click", function(){
 		  bugtest(this);
