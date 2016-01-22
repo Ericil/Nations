4a4afa29bde53989ae88c3cb1ed988599606363c
Sally <sallybao29@gmail.com>
2016-01-22 00:11:06 -0500

    fixing transitions

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("test.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
--- static/build.js
+++ static/build.js
@@ -1,13 +1,13 @@
 console.log("Here is the build");
 
+var len = 14;
 var pos = 0;
 
 /*Make the bar appear or disappear
  */
 var build = function build(){
-		$("#build-bar").animate({
-				width: 'toggle'
-		});
+		var bar = document.getElementById("build-bar");
+		bar.classList.toggle("slidein");
 };
 
 
@@ -32,35 +32,41 @@ var enable = function enable(name){
  */
 var slidePrevious = function slidePrevious(){
 		pos -= 6;
-		var panels = document.getElementsByClassName("build-panel");
-		for (var i = pos; i >= 0; i--){
-				panels[i].classList.remove("next");
-				panels[i].children[0].classList.remove("header-next");
-		}
+		var button = document.getElementById("build-previous");
 		
-		if (pos <= 6){
+		if (pos < 0) {
 				pos = 0;
 				disable("build-previous");
 				enable("build-next");
 		}
+		var elTop = $(".build-panel").eq(pos).position().top + button.style.height;
+		$(".build-group").animate({scrollTop: elTop}, 500);
 };
 
 /*Show the next 6 choices
  */
 var slideNext = function slideNext(){
 		pos += 6;
-		var panels = document.getElementsByClassName("build-panel");
-		for (var i = 0; i < panels.length && i < pos; i++){
-				panels[i].children[0].classList.add("header-next");
-				panels[i].classList.add("next");
-		}
+		var diff = len - pos;
+		var button = document.getElementById("build-previous");
 		
-		if (pos + 6 >= panels.length){
-				pos = panels.length;
+		if (diff < 6) {
+				pos = pos - (6 - diff);
 				disable("build-next");
 				enable("build-previous");
 		}
+		var elTop = $(".build-panel").eq(2).offset().top + button.style.height;
+		$(".build-group").animate({scrollTop: elTop}, 500);
 };
+
+
+/*Set multiple attributes for an element at once
+ */
+function setAttributes(element, attributes){
+		Object.keys(attributes).forEach(function(name){
+				element.setAttribute(name, attributes[name]);
+		});
+}
 
 /*Add click events listeners to buttons
  */
@@ -71,7 +77,23 @@ var setupBuild = function setupBuild(){
 		
 		var next = document.getElementById("build-next");
 		next.addEventListener("click", slideNext);
-}
+
+		var buildGroup = document.getElementsByClassName("build-group")[0];
+		for (var i = 0; i < 14; i++){
+				var panel = document.createElement("div");
+				setAttributes(panel,{
+						"class": "build-panel",
+						"title": "House",
+						"data-toggle": "popover",
+						"data-placement": "left",
+						"data-trigger": "hover",
+						"data-content": "$100,000"
+				});
+				buildGroup.appendChild(panel);
+		}
+		$('[data-toggle="popover"]').popover();
+		
+};
 
 
 
