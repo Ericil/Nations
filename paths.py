3dbfa9518f867e40e3f4745ded4d3475819254b2
CookieCutter <mingrui.li3@gmail.com>
2016-01-23 13:25:35 -0500

    updated alot of stuff, fixed some errors, most functions done

--- paths.py
+++ paths.py
@@ -1,5 +1,5 @@
 import utils, other 
-form flask import FLask, render_template, request, redirect, url_for
+from flask import FLask, render_template, request, redirect, url_for
 app = Flask(__name__)
 @app.route("/")
 def intro():
@@ -13,7 +13,7 @@ def intro():
 def login():
     if str(request.form["button"]) == "Log in!":
         username = str(request.form["username"])
-        if utils.pwordAuth(username, str(request.form["password"])): //need a utils.pwordAuth
+        if utils.pwordAuth(username, str(request.form["password"])): 
             return redirect('/play/' + username)
         else:
             return render_template("/login.html", text = "Username/Password does not match")
@@ -22,11 +22,11 @@ def login():
 
 """<-------------------------------REGISTER------------------------------->"""    
 @app.route("/register", methods = ["GET", "POST"])
-def register:
+def register():
     if str(request.form["button"]) == "Register!":
-        if utils.unameAuth(str(request.form["username"])) != True: //need a utils.unameAuth
-            utils.addAccount(str(request.form["username"]), str(request.form["password"]), str(request.form["firstname"]), str(request.form["lastname"])) //need utils.addAccount
-            utils.editInfo(str(request.form["username"]), str(request.form["paragraph_text"])) //need utils.addAcount
+        if utils.unameAuth(str(request.form["username"])) != True: 
+            utils.addAccount(str(request.form["username"]), str(request.form["password"]), str(request.form["firstname"]), str(request.form["lastname"]))
+            utils.editInfo(str(request.form["username"]), str(request.form["paragraph_text"])) 
             return redirect('/loginfinished/' + str(request.form["username"]))
         else:
             return render_template("/register.html", text = "this username already exists")
@@ -35,10 +35,10 @@ def register:
 
 """<-------------------------------SETTINGS------------------------------->"""
 @app.route("/settings")
-def settings:
+def settings():
     return redirect("/login")
-@app.route("/settings/<username>", methods = ["GET, POST"]) //make sure to make a pass confirmation
-def settings2:
+@app.route("/settings/<username>", methods = ["GET, POST"])
+def settings2():
     print("1")
     if request.method == "POST":
         print("2")
@@ -54,18 +54,18 @@ def settings2:
         return render_template("settings.html", username = username, friendslist = utils.friendList(username))
 
 
-"""<-------------------------------PLAY------------------------------->"""
+#PLAY
 @app.route("/play")
-def play:
+def play():
     return redirect("/login")
-@app.route("/play/<username>", methods = ["GET", "POST"]) //make sure to make a pass confirmation
-def play2:
+@app.route("/play/<username>", methods = ["GET", "POST"]) 
+def play2():
     return render_template("/test.html", username = username)
 
 @app.route("/loginfinished/<username>", methods = ["GET", "POST"])
-def logfin:
-
-"""<-------------------------------GET_FUNCTIONS------------------------------->"""
+def logfin():
+    return "hello"
+#GET_FUNCTIONS
 @app.route("/get_functions", methods = ["GET", "POST"])
 def get_functions():
     function_type = request.form["type"]
@@ -73,40 +73,50 @@ def get_functions():
     b = request.form["b"]
     c = request.form["d"]
     if function_type == "get_accountID":
-        """username""":
+        """username"""
         hold == utils.findID(a)
         return hold
 
-    if function_type == "get_cityIDs"
+    if function_type == "get_cityIDs":
         """accountID"""
+        accountID = utils.findID(a)
         hold = utils.getCitiesID(a)
         return hold
 
     if function_type == "get_resources":
-        """cityID"""
-        hold = utils.getResources(a)
+        """cityName"""
+        cityID = utils.getCityID(a)
+        hold = utils.getResources(cityID)
         return hold
 
     if function_type == "get_multipliers":
-        """cityID"""
-        return "multipliers"
-
+        """cityName"""
+        cityID = utils.getCityID(a)
+        hold = utils.getResourceIncreases(cityID)
+        return hold
+        
     if function_type == "get_msgs":
         """from, to"""
-        hold = utils.getmsgs(a, b)
+        accountID = utils.findID(a)
+        friendID = utils.findID(b)
+        hold = utils.getmsgs(accountID, friendID)
         return hold
 
-    if function_type == "get_citymap":
+    if function_type == "get_city_on_map":
         """mapx, mapy"""
-        hold = utils.getCity(a, b) 
+        hold = utils.getCity(a, b)
+        return hold
         
     if function_type == "get_city_buildings":
-        """cityID"""
-        hold = utils.getBuildingsIn(a)
+        """cityName"""
+        cityID = utils.getCityID(a)
+        hold = utils.getBuildingsIn(cityID)
+        return hold
 
     if function_type == "get_specific_building":
-        """cityID, buildingx, buildingy"""
-        hold = utils.getBuildingXY(a, b, c)
+        """cityName, buildingx, buildingy"""
+        cityID = utils.getCityID(a)
+        hold = utils.getBuildingXY(cityID, b, c)
         return hold
 
     if function_type == "get_specific_building_stat":
@@ -116,7 +126,8 @@ def get_functions():
 
     if function_type == "get_friends":
         """username"""
-        hold = utils.getFriends(a)
+        accountID = utils.findID(a)
+        hold = utils.getFriends(accountID)
         return hold
 
     if function_type == "base_building_stats":
@@ -127,44 +138,54 @@ def get_functions():
 """<-------------------------------SET_FUNCTIONS------------------------------->"""
 @app.route("/set_functions", methods = ["GET", "POST"])
 def set_functions():
-    function_type = request.fomr["type"]
-        a = request.form["a"]
-        b = request.form["b"]
-        c = request.form["c"]
-        d = request.form["d"]
-        e = request.form["e"]
-        f = request.form["f"]
-        g = request.form["g"]
+    function_type = request.form["type"]
+    a = request.form["a"]
+    b = request.form["b"]
+    c = request.form["c"]
+    d = request.form["d"]
+    e = request.form["e"]
+    f = request.form["f"]
+    g = request.form["g"]
     if function_type == "add_building":
-        """cityID, buildingx, buildingy, buildingtype"""
-        utils.addBuilding(a, b, c, d)
+        """cityName, buildingx, buildingy, buildingtype"""
+        cityID = utils.getCityID(a)
+        utils.addBuilding(cityID, b, c, d)
 
     if function_type == "update_resources":
-        """cityid, wood, iron, gold, food, population, soldiers"""
-       utils.updateResources(a, b, c, d, e, f, g)
+        """cityName, wood, iron, gold, food, population, soldiers"""
+        cityID = utils.getCityID(a)
+        utils.updateResources(cityID, b, c, d, e, f, g)
 
-    if function_type == "set_multipliers":
+    #if function_type == "set_multipliers":
         
 
     if function_type == "set_msgs":
-        """fromID, toID, messages"""
-        utils.addmsg(a, b, c)
+        """from, to, messages"""
+        accountID = utils.findID(a)
+        friendID = utils.findID(b)
+        utils.addmsg(accountID, friendID, c)
 
     if function_type == "set_friends":
-        """userID, friendID"""
-        utils.addFriend(a, b)
+        """username, friend"""
+        accountID = utils.findID(a)
+        friendID = utils.findID(b)
+        utils.addFriend(accountID, friendID)
 
     if function_type == "add_city":
         """cityname, cx, cy, wood, iron, gold, food"""
         utils.addCity(a, b, c, d, e, f, g)
 
     if function_type == "set_city_owner":
-        """accountID, cityID"""
-        utils.setCityOwner(a, b)
+        """username, cityname"""
+        accountID = utils.findID(a)
+        cityID = utils.getCityID(b)
+        utils.setCityOwner(accountID, cityID)
 
     if function_type == "set_building":
-        //update a building
+        """buildingID"""
+        utils.LevelUpBuilding(a);
+        
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     app.debug = True
     app.run()
